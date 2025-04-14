5db8d4a8151ec150cedbddea9bdb95760a7aa8d9
Xing320823 <3476134326@qq.com>
2025-04-14 17:17:10 +0800

    ref与reactive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,18 +2,23 @@
     <div class="person">
         <h2>一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
         <button @click="changePrice">修改汽车的价格</button>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum">点我sum+1</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-    import {reactive} from 'vue'
+    import {ref,reactive} from 'vue'
     //数据
     let car = reactive({brand:'奔驰',price:100})
+    let sum = ref(0)
 
     //方法
     function changePrice(){
         car.price += 10
-        console.log(car.price)
+    }
+    function changeSum(){
+        sum.value += 1
     }
 </script>
 
