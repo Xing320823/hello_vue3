bd2af99dd4e73cfcf98469f2cc1069ebcae31227
Xing320823 <3476134326@qq.com>
2025-04-14 17:36:45 +0800

    toRefs

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,26 +1,31 @@
 <template>
     <div class="person">
-        <h2>一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-        <button @click="changePrice">修改汽车的价格</button>
-        <h2>当前求和为：{{ sum }}</h2>
-        <button @click="changeSum">点我sum+1</button>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-    import {ref,reactive} from 'vue'
-    //数据
-    let car = reactive({brand:'奔驰',price:100})
-    let sum = ref(0)
+    import {reactive,toRefs} from 'vue'
+    let person = reactive({
+        name:'张三',
+        age:18
+    })
 
-    //方法
-    function changePrice(){
-        car.price += 10
+    let {name,age} = toRefs(person)
+
+    function changeName(){
+        name.value += '~'
     }
-    function changeSum(){
-        sum.value += 1
+    function changeAge(){
+        age.value +=1
     }
+
+
 </script>
+
 
 <style scoped>
     .person {
