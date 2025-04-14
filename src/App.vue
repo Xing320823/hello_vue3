4f02ad5e535300c8b56b2f1169012f773ad3545a
Xing320823 <3476134326@qq.com>
2025-04-14 15:07:52 +0800

    setup概述

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-    <div class="app">
-        <h1>
-            你好啊！
-        </h1>
-    </div>
+    <Person/>
 </template>
 
 <script lang="ts">
+    import Person from './components/Person.vue'
     export default {
-        name:'App' //组件名
+        name:'App', //组件名
+        components:{Person} //注册组件
     }
 </script>
 
 <style>
-    .app {
-        background-color: #ddd;
-        box-shadow: 0 0 10px;
-        border-radius: 10px;
-        padding: 20px;
-    }
+
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Person',
+        setup(){
+            //数据
+            let name = '张三'//name不是响应式的
+            let age = 18
+            let tel = '13888888888'
+            //方法
+            function changeName(){
+                name = 'zhang-san'
+                console.log('1')//输出1，按钮有效的，只是值不是响应的
+            }
+            function changeAge(){
+                age+=1
+            }
+            function showTel(){
+                alert(tel)
+            }
+
+            return {name,age,changeName,changeAge,showTel}
+        }
+    }
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 0 5px;
+    }
+</style>
